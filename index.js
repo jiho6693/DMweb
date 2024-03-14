6f4b7e1beb1578809cac41672891a709dde96cdb
jiho6693 <56946702+jiho6693@users.noreply.github.com>
2024-03-14 10:24:36 -0400

    a

--- index.js
+++ index.js
@@ -15,19 +15,4 @@ gotop();
 
 
 
-// fullscreen.js 파일에 추가
-const videoContainer = document.querySelector('.main');
-const videoIframe = videoContainer.querySelector('iframe');
 
-videoContainer.addEventListener('click', () => {
-    // 비메오 비디오를 전체 화면으로 변경합니다.
-    if (videoIframe.requestFullscreen) {
-        videoIframe.requestFullscreen();
-    } else if (videoIframe.mozRequestFullScreen) { // Firefox
-        videoIframe.mozRequestFullScreen();
-    } else if (videoIframe.webkitRequestFullscreen) { // Chrome, Safari 및 Opera
-        videoIframe.webkitRequestFullscreen();
-    } else if (videoIframe.msRequestFullscreen) { // IE/Edge
-        videoIframe.msRequestFullscreen();
-    }
-});
